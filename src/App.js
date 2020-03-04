d1710c2fdc7cf1e8dbb7540a99e0da0dcbe8b06e
sharath <sharath.r@mountblue.io>
2020-03-04 10:56:03 +0000

    aws cognito auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,172 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Amplify, { Auth } from 'aws-amplify'
 import './App.css';
+import config from './aws-exports';
 
 function App() {
+  Amplify.configure(config);
+  const [userName,setUserName] = useState('');
+  const [loginEmail, setLoginEmail] = useState('');
+  const [signUpEmail, setSignUpEmail] = useState('');
+  const [loginMobileNumber, setLoginMobileNumber] = useState('');
+  const [signUpMobileNumber, setSignUpMobileNumber] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const [signUpPassword, setSignUpPassword] = useState('');
+  const [otp, setOtp] = useState('');
+  const [emailOtp, setEmailOtp] = useState('');
+
+  const handleSignin = async () => {
+    console.log(loginEmail, loginMobileNumber, loginPassword);
+    if(loginEmail.length !== 0) {
+      try {
+        const user = await Auth.signIn(loginEmail, loginPassword);
+        console.log(user);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      try {
+        const user = await Auth.signIn(loginMobileNumber, loginPassword);
+        console.log(user);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  const handleSignUp = async () => {
+    console.log(signUpEmail, signUpMobileNumber, signUpPassword);
+    try {
+      const user = await Auth.signUp({
+        username: userName,
+        password: signUpPassword,
+        attributes: {
+          email: signUpEmail,
+          phone_number: signUpMobileNumber
+        }
+      });
+      console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleVerify = async () => {
+    console.log(otp);
+    try {
+      const data = await Auth.confirmSignUp(userName, otp, { forceAliasCreation: true });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const sendOtpToEmail = () => {
+    Auth.verifyCurrentUserAttribute("email")
+      .then(() => {
+        console.log('a verification code is sent');
+      }).catch((e) => {
+        console.log('failed with error', e);
+      });
+  }
+
+  const verfiyEmail = () => {
+    Auth.verifyCurrentUserAttributeSubmit("email", emailOtp)
+      .then(() => {
+        console.log('signUpEmail verified');
+      }).catch(e => {
+        console.log('failed with error', e);
+      });
+  }
+
+  const SignUp = () => (
+    <div className="container">
+        <h3>Signup</h3>
+        <div className="tableDiv">
+          <table>
+            <tr>
+              <td>Username</td>
+              <td><input type="text" onChange={(e) => setUserName(e.target.value)} value={userName}/></td>
+            </tr>
+            <tr>
+              <td>Mobile Number</td>
+              <td><input type="text" onChange={(e) => setSignUpMobileNumber(e.target.value)} value={signUpMobileNumber}/></td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td><input type="email" onChange={(e) => setSignUpEmail(e.target.value)} value={signUpEmail}/></td>
+            </tr>
+            <tr>
+              <td>Password</td>
+              <td><input type="password" onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword}/></td>
+            </tr>
+          </table>
+          <button type="button" onClick={handleSignUp}>Sign Up</button>
+        </div>
+      </div>
+  );
+
+  const MobileNumberVerification = () => (
+    <div className="container">
+        <h3>Phone Number Verification</h3>
+        <div className="tableDiv">
+          <table>
+            <tr>
+              <td>OTP</td>
+              <td><input type="text" onChange={(e) => setOtp(e.target.value)} value={otp}/></td>
+            </tr>
+          </table>
+        </div>
+        <div><button type="button" onClick={handleVerify}>Verify</button></div>
+      </div>
+  )
+
+  const Login = () => (
+    <div className="container">
+        <h3>Login</h3>
+        <h6>You can login with Email if it is verified or you can use Mobile number</h6>
+        <div className="tableDiv">
+        <table>
+          <tr>
+            <td>Mobile number</td>
+            <td><input type="text" onChange={(e) => setLoginMobileNumber(e.target.value)} value={loginMobileNumber}/></td>
+          </tr>
+            <td>Email</td>
+            <td><input type="email" onChange={(e) => setLoginEmail(e.target.value)} value={loginEmail}/></td>
+          <tr></tr>
+            <td>Password</td>
+            <td><input type="password" onChange={(e) => setLoginPassword(e.target.value)} value={loginPassword} /></td>
+          <tr></tr>
+        </table>
+        <button type="button" onClick={handleSignin}>Sign In</button>
+        </div>
+      </div>  
+  );
+
+  const EmailVerification = () => (
+    <div className="container">
+        <h3>Email Verification</h3>
+        <h6>After login only email can be verified</h6>
+        <div>
+        <button type="button" onClick={sendOtpToEmail}>send OTP to email</button></div>
+        <div className="tableDiv">
+          <table>
+            <tr>
+              <td>OTP</td>
+              <td><input type="text" onChange={(e) => setEmailOtp(e.target.value)} value={emailOtp}/></td>
+            </tr>
+          </table>
+          <button type="button" onClick={verfiyEmail}>Verify</button>
+        </div>
+      </div>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SignUp />
+      <MobileNumberVerification />
+      <Login />
+      <EmailVerification />
     </div>
   );
 }
---
+++ src/aws-exports.js
@@ -0,0 +1,8 @@
+const config = {
+    Auth: {identityPoolId: 'ap-south-1:6952e48e-0039-443a-92ed-17efdf40c6d0',
+    region: 'ap-south-1',
+    userPoolId: 'ap-south-1_zw7O3r3SQ',
+    userPoolWebClientId: '3g05j1bb5vuuqotecu714cirgi'}
+  }
+  
+  export default config;
